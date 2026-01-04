582042375139b2155033a34482da3ef20720985f
koghalai123 <52839926+koghalai123@users.noreply.github.com>
2026-01-04 14:49:13 -0800

    Create install script for ROS 2 Jazzy
    This script installs ROS 2 Jazzy by setting up locales, adding repositories, and installing necessary packages.

---
+++ installROS2Jazzy.sh
@@ -0,0 +1,31 @@
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
+sudo apt update && sudo apt install ros-dev-tools
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install ros-jazzy-desktop
+
+source /opt/ros/jazzy/setup.bash
+
+#source /opt/ros/jazzy/setup.bash
+#ros2 run demo_nodes_cpp talker
+
+#source /opt/ros/jazzy/setup.bash
+#ros2 run demo_nodes_py listener
